fe8f18aa1cb2c19f6aab15e7d2367161117b64c0
lasbo <mathis.henry@edu.ece.fr>
2024-05-30 18:10:52 +0200

    MAJ RECHERCHE

--- recherche.php
+++ recherche.php
@@ -25,32 +25,70 @@ if ($db_found) {
             // Exécuter la requête SQL
             $result = mysqli_query($db_handle, $sql);
             
-            // Vérifier si des résultats ont été trouvés
-            if (mysqli_num_rows($result) > 0) {
-                echo '<div class="resultats-recherche">';
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo '<div class="resultat">';
-                    // Afficher la photo du coach
-                    if ($row["photo"]) {
-                        echo '<img src="' . $row["photo"] . '" alt="Photo de ' . $row["prenom"] . '" width="100"><br>';
-                    }
-                    // Afficher le nom et prénom du coach avec la couleur spécifiée
-                    echo '<h2 style="color: #007179;">' . $row["prenom"] . ' ' . $row["nom"] . '</h2>';
-                    // Afficher la spécialité
-                    echo '<p>Spécialité: ' . $row["specialite"] . '</p>';
-                    // Afficher la salle
-                    echo '</div>';
-                }
-                echo '</div>';
-            } else {
-                echo "Aucun coach trouvé.";
-            }
+
         }
     }
-} else {
-    echo "Base de données introuvable.";
 }
 
 // Fermer la connexion à la base de données
 mysqli_close($db_handle);
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RDV - Sportify</title>
+    <link rel="stylesheet" href="recherche.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <style>
+
+    </style>
+</head>
+<body>
+<header>
+    <h1>Sportify</h1>
+    <nav class="navbar navbar-expand-md">
+        <img class="navbar-brand" src="imgs/logo.png" alt="logo" style="width:40px;">
+        <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-navigation">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="accueil.html">Accueil</a></li>
+                <li class="nav-item"><a class="nav-link" href="parcourir.html">Tout Parcourir</a></li>
+                <li class="nav-item"><a class="nav-link" href="parcourir.html">Recherche</a></li>
+                <li class="nav-item"><a class="nav-link" href="RDV.html">RDV</a></li>
+                <li class="nav-item"><a class="nav-link" href="login.html">Connexion</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<body>
+    <h1> RESULTATS </h1>
+        <?php
+        if (mysqli_num_rows($result) > 0) {
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<div class="recherche">';
+                // Afficher la photo du coach
+                if ($row["photo"]) {
+                    echo '<img src="' . $row["photo"] . '" alt="Photo de ' . $row["prenom"] . '" width="100"><br>';
+                }
+                // Afficher le nom et prénom du coach avec la couleur spécifiée
+                echo '<h2 style="color: #007179;">' . $row["prenom"] . ' ' . $row["nom"] . '</h2>';
+                // Afficher la spécialité
+                echo '<p>Spécialité: ' . $row["specialite"] . '</p>';
+                // Afficher la salle
+                echo '</div>';
+            }
+        }
+        else {
+            echo "Aucun coach trouvé.";
+        }
+        ?>
+
+</body>
+</html>
